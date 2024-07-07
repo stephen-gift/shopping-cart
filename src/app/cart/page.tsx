bf875cb55d1e07070ee65efbaa074e80ed846495
Stephen <79639335+stephen-gift@users.noreply.github.com>
2024-07-07 14:20:12 +0100

    Cart Page

---
+++ src/app/cart/page.tsx
@@ -0,0 +1,14 @@
+import { SCCartPageContainer } from "@/containers";
+import React from "react";
+
+type Props = {};
+
+const page = (props: Props) => {
+  return (
+    <>
+      <SCCartPageContainer />
+    </>
+  );
+};
+
+export default page;
