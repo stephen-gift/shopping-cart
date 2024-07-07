babd546c7d28d5e7b198f0c545fc63723d6d10fa
Stephen <79639335+stephen-gift@users.noreply.github.com>
2024-07-07 14:20:26 +0100

    Components

---
+++ src/components/index.tsx
@@ -0,0 +1,37 @@
+import SCButton from "./global/Button";
+import SCProductCard from "./global/Card";
+import SCProductList from "./global/Card/SCProductList";
+import SCFooter from "./global/Footer";
+import SCHeader from "./global/Header";
+import SCInput from "./global/Input";
+import SCCustomSearchInput from "./global/Input/CustomSearchInput";
+import SCInputErrorMessage from "./global/Input/InputErrorMessage";
+import SCInputHelperText from "./global/Input/InputHelperText";
+import SCInputLabel from "./global/Input/InputLabel";
+import SCMainLayout from "./global/Layout";
+import SCImageSlider from "./global/Carousel";
+import SCCarousel from "./global/Carousel";
+import SCCart from "./global/Cart";
+import SCCustomBreadCrumbs from "./global/BreadCrumb";
+import SCCustomCarousel from "./global/Carousel";
+import SCSeparatorIcon from "./global/CustomIcon/SCSeparatorIcon";
+
+export {
+  SCButton,
+  SCInput,
+  SCInputErrorMessage,
+  SCInputHelperText,
+  SCInputLabel,
+  SCCustomSearchInput,
+  SCProductCard,
+  SCHeader,
+  SCFooter,
+  SCImageSlider,
+  SCProductList,
+  SCMainLayout,
+  SCCarousel,
+  SCCart,
+  SCCustomBreadCrumbs,
+  SCCustomCarousel,
+  SCSeparatorIcon,
+};
