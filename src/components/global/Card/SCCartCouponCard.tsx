38554406a83c8c3323d64d16bf27ca101233aa44
Stephen <79639335+stephen-gift@users.noreply.github.com>
2024-07-07 14:23:02 +0100

    Coupon card

---
+++ src/components/global/Card/SCCartCouponCard.tsx
@@ -0,0 +1,45 @@
+import { CheckIcon } from "@chakra-ui/icons";
+import {
+  Button,
+  Card,
+  CardBody,
+  CardHeader,
+  Heading,
+  Input,
+  InputGroup,
+  InputRightElement,
+  Text,
+} from "@chakra-ui/react";
+import React, { useState } from "react";
+
+type Props = {};
+
+const SCCartCouponCard = (props: Props) => {
+  const [clicked, setClicked] = useState(false);
+  const handleClick = () => setClicked(!clicked);
+  return (
+    <Card>
+      <CardHeader>
+        <Heading>Apply coupon</Heading>
+      </CardHeader>
+      <CardBody>
+        <InputGroup size="md">
+          <Input pr="4.5rem" type={"text"} placeholder="Enter password" />
+          <InputRightElement width="4.5rem">
+            <Button h="1.75rem" size="sm" onClick={handleClick} color={'red'} p={1}>
+              {clicked ? (
+                <Text>
+                  Applied <CheckIcon />
+                </Text>
+              ) : (
+                <Text>Apply</Text>
+              )}
+            </Button>
+          </InputRightElement>
+        </InputGroup>
+      </CardBody>
+    </Card>
+  );
+};
+
+export default SCCartCouponCard;
