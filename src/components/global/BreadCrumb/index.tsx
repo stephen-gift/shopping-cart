331fb290913d427c497f3d8313695a72980026a1
Stephen <79639335+stephen-gift@users.noreply.github.com>
2024-07-08 12:01:31 +0100

    update breadcrumbs

--- src/components/global/BreadCrumb/index.tsx
+++ src/components/global/BreadCrumb/index.tsx
@@ -3,6 +3,7 @@ import {
   BreadcrumbItem,
   BreadcrumbLink,
   Link,
+  Text,
 } from "@chakra-ui/react";
 import React from "react";
 import SCSeparatorIcon from "../CustomIcon/SCSeparatorIcon";
@@ -17,9 +18,28 @@ const SCCustomBreadCrumbs = ({ items }: BreadcrumbsProps) => {
       {items.map((item, index) => (
         <BreadcrumbItem key={index} isCurrentPage={item.isCurrentPage}>
           {item.href ? (
-            <BreadcrumbLink href={item.href}>{item.label}</BreadcrumbLink>
+            <BreadcrumbLink
+              href={item.href}
+              _hover={{ textDecoration: "none" }}
+            >
+              <Text
+                fontSize={{ base: "15px", md: "20px" }}
+                fontWeight={item.isCurrentPage ? "600" : "400"}
+                textAlign="left"
+              >
+                {item.label}
+              </Text>
+            </BreadcrumbLink>
           ) : (
-            <BreadcrumbLink>{item.label}</BreadcrumbLink>
+            <BreadcrumbLink _hover={{ textDecoration: "none" }}>
+              <Text
+                fontSize={{ base: "15px", md: "20px" }}
+                fontWeight={item.isCurrentPage ? "600" : "400"}
+                textAlign="left"
+              >
+                {item.label}
+              </Text>
+            </BreadcrumbLink>
           )}
         </BreadcrumbItem>
       ))}
