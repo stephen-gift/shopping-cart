5ec00270d0d1f4e62b2fd71a81f2e559e84bb128
Stephen <79639335+stephen-gift@users.noreply.github.com>
2024-07-08 12:16:43 +0100

    update SCTopProductsContainer

--- src/containers/HomePageContainer/TopProducts.tsx
+++ src/containers/HomePageContainer/TopProducts.tsx
@@ -8,16 +8,15 @@ import products from "@/data/products";
 
 type Props = {};
 
-
-
 const SCTopProductsContainer = (props: Props) => {
   return (
-    <Stack>
-      <Flex
-        justifyContent={{ base: "space-between"}}
-        alignItems="center"
-      >
-        <Heading size={{ base: "xs", sm: "md", md: "lg" }}>
+    <Stack p={{ base: 1, md: 5, lg: 7 }}>
+      <Flex justifyContent={{ base: "space-between" }} alignItems="center">
+        <Heading
+          fontSize={{ base: "18px", md: "23px", lg: "25px" }}
+          color={"#5A2C17"}
+          fontWeight={600}
+        >
           Top Products
         </Heading>
         <Flex justifyContent="center" alignItems="center" gap={3}>
