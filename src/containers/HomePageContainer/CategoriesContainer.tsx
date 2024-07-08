0274dc5e388b868325a33320f16f26776b9eaeab
Stephen <79639335+stephen-gift@users.noreply.github.com>
2024-07-08 12:16:12 +0100

    update SCCategoriesContainer

--- src/containers/HomePageContainer/CategoriesContainer.tsx
+++ src/containers/HomePageContainer/CategoriesContainer.tsx
@@ -8,7 +8,7 @@ const SCCategoriesContainer = (props: Props) => {
   return (
     <Flex
       direction="row"
-      justifyContent="center"
+      justifyContent="space-evenly"
       alignItems="center"
       wrap="wrap"
       gap="4"
@@ -20,7 +20,6 @@ const SCCategoriesContainer = (props: Props) => {
           href={`#${category.toLowerCase().replace(/ /g, "-")}`} // Assuming you want the links to scroll to sections with corresponding ids
           fontSize={{ base: "14px", md: "18px" }}
           fontWeight="500"
-          lineHeight={{ base: "21px", md: "27px" }}
           textAlign="left"
           color="#5A2C17"
           _hover={{ textDecoration: "none", color: "#374382" }} // Optional: add hover styles
