5d19aaeadd7b1e7ee10f4bce0d6e0fdf4e7f69c4
Stephen <79639335+stephen-gift@users.noreply.github.com>
2024-07-09 21:51:19 +0100

    update

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,7 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import { Providers } from "./providers";
-// import "@/styles/globals.css";
+import "@/styles/globals.css";
 import { Inter as FontSans } from "next/font/google";
 import { cn } from "@/lib/utils";
 
